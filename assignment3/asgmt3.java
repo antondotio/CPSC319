5713a6dc6c6c0d7088e8f239fc1c1cacafbeb1eb
Anton Santos <43733827+antondotio@users.noreply.github.com>
2019-03-16 03:16:04 -0600

    .

--- assignment3/asgmt3.java
+++ assignment3/asgmt3.java
@@ -7,6 +7,7 @@ public class asgmt3{
         Node root;
         int counter = 0;
         int numUniqueWords = 0;
+        int mostFreq = 0;
 
         private static class Node{
             String word;
@@ -43,14 +44,14 @@ public class asgmt3{
             if(curr == null)
                 return;
             inOrder(curr.left);
-            System.out.print(curr.word + "  ");
+            System.out.print(curr.word + " ");
             inOrder(curr.right);
         }
 
         public void preOrder(Node curr){
             if(curr == null)
                 return;
-            System.out.print(curr.word + "  ");
+            System.out.print(curr.word + " ");
             preOrder(curr.left);
             preOrder(curr.right);
         }
@@ -60,7 +61,7 @@ public class asgmt3{
                 return;
             postOrder(curr.left);
             postOrder(curr.right);
-            System.out.print(curr.word + "  ");
+            System.out.print(curr.word + " ");
         }
 
         public int maximumDepth(Node curr){
@@ -77,22 +78,44 @@ public class asgmt3{
             }
         }
 
-        public void numOfWordsTraverse(Node curr){
-            if(curr == null)
-                return;
-            counter++;
-            numOfWordsTraverse(curr.left);
-            numOfWordsTraverse(curr.right);
-        }
-
-        public void uniqueWordsTraverse(Node curr){
+        public void initialTraverse(Node curr){
             if(curr == null)
                 return;
             if(curr.frequency == 1){
                 numUniqueWords++;
             }
-            uniqueWordsTraverse(curr.left);
-            uniqueWordsTraverse(curr.right);
+            if(curr.frequency > mostFreq){
+                mostFreq = curr.frequency;
+            }
+            counter++;
+            
+            initialTraverse(curr.left);
+            initialTraverse(curr.right);
+        }
+
+        public void printFrequency(Node curr){
+            if(curr == null)
+                return;
+             if(curr.frequency == mostFreq){
+                System.out.println(curr.word + " = " + mostFreq + " times");
+            }
+            printFrequency(curr.left);
+            printFrequency(curr.right);  
+           
+        }
+
+        public boolean lookFor(Node curr, String w){
+            if(curr == null)
+                return false;
+            if(curr.word.equals(w)){
+                System.out.println("Found! It appears " + curr.frequency + " times");
+                return true;
+            }else{
+                lookFor(curr.left, w);
+                lookFor(curr.right, w);
+                
+                return false;
+            }
         }
 
     }
@@ -104,42 +127,47 @@ public class asgmt3{
     }
 
     public static void initialOutput(BinaryTree bnt){
-        bnt.numOfWordsTraverse(bnt.root);
+        // bnt.numOfWordsTraverse(bnt.root);
+        bnt.initialTraverse(bnt.root);
+
         System.out.println("Total number of words is: " + bnt.counter);
-
-        bnt.uniqueWordsTraverse(bnt.root);
         System.out.println("Number of unique words is: " + bnt.numUniqueWords);
-
+        
+        System.out.println("The word(s) that occur(s) most often and the number of times it/they occur(s): ");
+        bnt.printFrequency(bnt.root);
         
         System.out.println("The maximum height of the tree is: " + bnt.maximumDepth(bnt.root));
 
     }
 
+    public static void search(BinaryTree bnt, String word){
+        boolean found = bnt.lookFor(bnt.root, word);
+        if(found == false){ //TODO: fix this
+            System.out.println("Word not found");
+        }
+    }
+
     public static String[] readFile(String file){
         File text = null;
-        BufferedReader br = null;
+        Scanner scan = null;
         String sentence = "";
         try{
             text = new File(file);
-            br = new BufferedReader(new FileReader(text));
-            sentence += br.readLine() + " ";
-            while(br.readLine() != null){
-                sentence += br.readLine() + " ";
-                System.out.println(br.readLine());
-
+            scan = new Scanner(new FileReader(text));
+            while(scan.hasNext()){
+                sentence += scan.nextLine() + " ";
             } 
         }catch(Exception e) {
 			e.printStackTrace();
 		}finally {
 			try {
-				if (br != null) {
-					br.close();
+				if (scan != null) {
+					scan.close();
 				}
 			} catch (Exception e2) {
 				e2.printStackTrace();
 			}
         }
-        System.out.println(sentence);
         //This removes all punctuations, turns everything into lowercase and splits it by the spaces
         String[] words = sentence.replaceAll("\\p{P}", "").toLowerCase().split("\\s+");
 
@@ -154,7 +182,46 @@ public class asgmt3{
 
         createBinaryTree(bnt, words); 
         initialOutput(bnt);
+        
+        boolean b = true;
+        while(b){
+            System.out.println("\nWhat else would you like to do?\n"
+                                + "\t1. Find a word\n"
+                                + "\t2. Print binary tree IN-ORDER\n"
+                                + "\t3. Print binary tree PRE-ORDER\n"
+                                + "\t4. Print binary tree POST-ORDER\n"
+                                + "\t5. Quit");
+            int choice = scan.nextInt();
+            scan.nextLine();
+            switch(choice){
+                case 1:
+                    System.out.print("What word are you looking for? ");
+                    String find = scan.nextLine();
+                    search(bnt, find);
+                    break;
+                case 2: 
+                    System.out.println("\nPrinting the binary tree IN-ORDER...");
+                    bnt.inOrder(bnt.root);
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println("\nPrinting the binary tree PRE-ORDER...");
+                    bnt.preOrder(bnt.root);
+                    System.out.println();
+                    break;
+                case 4:
+                    System.out.println("\nPrinting the binary tree POST-ORDER...");
+                    bnt.postOrder(bnt.root);
+                    System.out.println();
+                    break;
+                case 5:
+                    
+                    System.out.println("Ending Program");
+                    b = false;
+                    break;
+            }
+        }
 
-        scan.close();
+            scan.close();
     }
 }
